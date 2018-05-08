de9f78aaaded146632c512d69a4c7f88b6498cd1
Connor Bird <cpbird90@gmail.com>
2018-05-07 22:38:55 -0400

    Update

---
+++ SimpleContacts/App_Data/dbo.Contacts.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[ContactID]   INT  IDENTITY(1,1) PRIMARY KEY,
+	[FirstName] NVARCHAR (256) NULL,
+	[LastName] NVARCHAR (256) NULL,
+	[Note] TEXT NULL,
+	[StreetAddress] VARCHAR(256) NULL,
+	[CityAddress] VARCHAR (128) NULL,
+	[StateAddress] VARCHAR (2) NULL,
+	[ZipAddress] VARCHAR (10) NULL,
+	[Email] NVARCHAR (128) NULL,
+	[Phone] INT NULL
+	)
